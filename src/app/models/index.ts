f80416066d265568a20b320b5b9f8eb9b0114613
rajudangol <dangol.raju.work@gmail.com>
2025-12-13 17:05:23 +0545

    feat: Add required models
    Driver, Hub, Order, VehicleAllocation, Vehicle Interfaces add

---
+++ src/app/models/index.ts
@@ -0,0 +1,5 @@
+export * from './hub.interface';
+export * from './driver.interface';
+export * from './vehicle.interface';
+export * from './order.interface';
+export * from './vehicle-allocation.interface';
---
+++ src/app/models/vehicle-allocation.interface.ts
@@ -0,0 +1,6 @@
+export interface VehicleAllocation {
+  id: string;
+  vehicleId: string;
+  driverId: string;
+  date: string;
+}
