46f993cdb49c3b5eaa3fb8ad4d9296283c597133
rajudangol <dangol.raju.work@gmail.com>
2025-12-14 19:56:53 +0545

    Allow dynamic env files for production
    Add environment and environment.prod

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Hub, Driver, Vehicle, Order, VehicleAllocation } from '../models';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  private baseUrl = 'http://localhost:3000';
+  private baseUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
